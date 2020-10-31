4049f3d12f6ad80616272e6ca1681ee6ee457bc4
Christopher Booth <christopherbooth@ChristophersMBP.attlocal.net>
2020-10-31 10:10:09 -0500

    (merge) backup before the big merge

--- server/index.js
+++ server/index.js
@@ -10,12 +10,14 @@ const dotenv = require('dotenv');
 
 // const auth = require('./auth/authroute');
 const app = express();
+const http = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../dist'); // NEW
 const HTML_FILE = path.join(DIST_DIR, 'index.html'); // NEW
 // const cookieSession = require('cookie-session');
 const cors = require('cors');
 
+const io = require('socket.io')(http);
 // app.use(
 //   cookieSession({
 //     name: 'prost',
@@ -29,35 +31,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(express.static(DIST_DIR)); // NEW
 // app.use(googleAuth)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* DB Routes */
 const { customerRouter } = require('./routes/customer');
@@ -85,6 +58,15 @@ app.use('/db/menu', menuRouter);
 const { eContactRouter } = require('./routes/eContact');
 app.use('/db/eContact', eContactRouter);
 // app.use('/auth', auth);
+
+//SOCKET STUFF
+const connectedUsers = {};
+
+io.on('connection', (socket) => {
+  console.log(`new client connected : ${socket.id}`);
+  connectedUsers[socket.id] = socket.id
+  socket.emit('connection', null);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(HTML_FILE); // EDIT
@@ -117,6 +99,6 @@ const syncModels = async () => {
 connection();
 syncModels();
 
-app.listen(port, function () {
+http.listen(port, function () {
   console.log('App listening on port: ' + port);
 });
